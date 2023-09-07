eeee2bab6af5c9e063102813fadea04e195b28ad
pabloo-zam <PFZAMORA@Correo.url.edu.gt>
2023-09-06 22:29:33 -0600

    Creacion de clase nodo y modelo de usuario

--- Lab1_Estructura2/Controller/Usuario.cs
+++ Lab1_Estructura2/Controller/Usuario.cs
@@ -10,10 +10,10 @@ using Lab1_Estructura2.Model;
 
 namespace Lab1_Estructura2.Controller
 {
-    internal class Usuario : Controller
+    /*internal class Usuario : Controller
     {
         public static AVL arbol = new AVL();
         public static List<UsuarioModel> listaAspi = new List<UsuarioModel>();
 
-    }
+    }*/
 }
