f86e2236813a1e3effed38907dba7b4caaaf32e6
pabloo-zam <PFZAMORA@Correo.url.edu.gt>
2023-09-07 21:43:38 -0600

    Implemntacion de arbol

--- Lab1_Estructura2/Model/Arbol.cs
+++ Lab1_Estructura2/Model/Arbol.cs
@@ -91,7 +91,7 @@ namespace Lab1_Estructura2.Model
             }
             if (balance < -1)
             {
-                if (dato > node.der.dato)
+                if (dato.nombre.CompareTo(node.der.dato.nombre)<=1)
                     return RotarIzquierda(node);
                 else
                 {
@@ -101,6 +101,107 @@ namespace Lab1_Estructura2.Model
             }
 
             return node;
+        }
+        private Node EncontrarMinimo(Node node)
+        {
+            Node actual = node;
+            while (actual.iz != null)
+                actual = actual.iz;
+            return actual;
+        }
+        //FUNCION ELIMINAR
+        public Node Eliminar(Node node, UsuarioModel dato)
+        {
+            if (node == null)
+            {
+                return node;
+            }
+            //ELIMINACION RECURSIVA
+            if (dato.nombre.CompareTo(node.dato.nombre) < 0)
+            {
+                node.iz = Eliminar(node.iz, dato);
+            }
+            else if (dato.nombre.CompareTo(node.dato.nombre) > 0)
+            {
+                node.der = Eliminar(node.der, dato);
+            }
+            else
+            {
+                if ((node.iz == null) || (node.der == null))
+                {
+                    Node temp = (node.iz != null) ? node.iz : node.der;
+
+                    if (temp == null)
+                    {
+                        temp = node;
+                        node = null;
+                    }
+                    else
+                    {
+                        node = temp;
+                    }
+                }
+                else
+                {
+                    Node temp = EncontrarMinimo(node.der);
+                    node.dato = temp.dato;
+                    node.der = Eliminar(node.der, temp.dato);
+                }
+            }
+
+            if (node == null)
+                return node;
+
+            node.altura = 1 + Math.Max(Altura(node.iz), Altura(node.der));
+
+            int balance = FactorBalance(node);
+
+            // Casos de rotación
+            if (balance > 1)
+            {
+                if (FactorBalance(node.iz) >= 0)
+                    return RotarDerecha(node);
+                else
+                {
+                    node.iz = RotarIzquierda(node.iz);
+                    return RotarDerecha(node);
+                }
+            }
+            if (balance < -1)
+            {
+                if (FactorBalance(node.der) <= 0)
+                    return RotarIzquierda(node);
+                else
+                {
+                    node.der = RotarDerecha(node.der);
+                    return RotarIzquierda(node);
+                }
+            }
+
+            return node;
+        }
+        //FUNCION ACTUALIZAR 
+        public Node Actualizar(Node node, UsuarioModel oldData, UsuarioModel newData)
+        {
+            node = Eliminar(node, oldData);
+            node = Insertar(node, newData);
+            return node;
+        }
+
+        public Node Buscar(Node node, UsuarioModel dato)
+        {
+            if (node == null || node.dato == dato)
+                return node;
+
+            if (dato.nombre.CompareTo(node.dato.nombre)<0)
+                return Buscar(node.iz, dato);
+
+            return Buscar(node.der, dato);
+        }
+
+        public Node Buscar(UsuarioModel dato)
+        {
+            return Buscar(raiz, dato);
         }
     }
 
